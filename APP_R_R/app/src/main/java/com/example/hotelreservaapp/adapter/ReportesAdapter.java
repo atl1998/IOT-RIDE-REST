0b099a7e9d6fd04ed01b708319314d73a242a830
Daniel <a20212591@pucp.edu.pe>
2025-05-07 00:28:21 -0500

    Avance reportes superadmin v2

--- APP_R_R/app/src/main/java/com/example/hotelreservaapp/adapter/ReportesAdapter.java
+++ APP_R_R/app/src/main/java/com/example/hotelreservaapp/adapter/ReportesAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,45 +17,62 @@ import java.util.List;
 
 public class ReportesAdapter extends RecyclerView.Adapter<ReportesAdapter.ReporteViewHolder> {
 
-    private List<Reporte> lista;
+    private List<Reporte> listaReportes;
 
-    public ReportesAdapter(List<Reporte> lista) {
-        this.lista = lista;
-    }
-
-    public void actualizarLista(List<Reporte> nuevaLista) {
-        this.lista = nuevaLista;
-        notifyDataSetChanged();
+    public ReportesAdapter(List<Reporte> listaReportes) {
+        this.listaReportes = listaReportes;
     }
 
     @NonNull
     @Override
     public ReporteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View vista = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_reporte, parent, false);
+                .inflate(R.layout.item_reporte_reserva, parent, false);
         return new ReporteViewHolder(vista);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ReporteViewHolder holder, int position) {
-        Reporte reporte = lista.get(position);
-        String texto = "Hotel " + reporte.getHotel() +
-                ": " + reporte.getAccion() +
-                "\n" + reporte.getUsuario();
-        holder.tvRegistro.setText(texto);
+        Reporte r = listaReportes.get(position);
+        holder.tvHotel.setText(r.getHotel());
+        holder.tvCliente.setText("Reserva realizada por: " + r.getCliente());
+        holder.tvFecha.setText(r.getFecha());
+        holder.tvEstado.setText(r.getEstado());
+        switch (r.getEstado().toLowerCase()) {
+            case "confirmada":
+                holder.tvEstado.setTextColor(holder.itemView.getContext().getColor(R.color.estado_confirmado));
+                break;
+            case "cancelada":
+                holder.tvEstado.setTextColor(holder.itemView.getContext().getColor(R.color.estado_cancelado));
+                break;
+            default:
+                holder.tvEstado.setTextColor(holder.itemView.getContext().getColor(android.R.color.darker_gray));
+                break;
+        }
+        holder.ivHotel.setImageResource(r.getImagenResId());
     }
 
     @Override
     public int getItemCount() {
-        return lista.size();
+        return listaReportes.size();
     }
 
-    public static class ReporteViewHolder extends RecyclerView.ViewHolder {
-        TextView tvRegistro;
+    public void actualizarLista(List<Reporte> nuevaLista) {
+        this.listaReportes = nuevaLista;
+        notifyDataSetChanged();
+    }
+
+    static class ReporteViewHolder extends RecyclerView.ViewHolder {
+        TextView tvHotel, tvCliente, tvFecha, tvEstado;
+        ImageView ivHotel;
 
         public ReporteViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvRegistro = itemView.findViewById(R.id.tvRegistro);
+            tvHotel = itemView.findViewById(R.id.tvHotel);
+            tvCliente = itemView.findViewById(R.id.tvCliente);
+            tvFecha = itemView.findViewById(R.id.tvFecha);
+            tvEstado = itemView.findViewById(R.id.tvEstado);
+            ivHotel = itemView.findViewById(R.id.ivHotel);
         }
     }
 }
