692c01af728995d062fdef2ba2d024d284b41ea8
Vinniellf <a20213736@pucp.edu.pe>
2025-05-08 14:50:32 -0500

    vista de servicios en adminhotel

--- APP_R_R/app/src/main/java/com/example/hotelreservaapp/AdminHotel/PerfilFragment.java
+++ APP_R_R/app/src/main/java/com/example/hotelreservaapp/AdminHotel/PerfilFragment.java
@@ -1,5 +1,6 @@
 package com.example.hotelreservaapp.AdminHotel;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,9 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.hotelreservaapp.MainActivity;
 import com.example.hotelreservaapp.R;
+import com.example.hotelreservaapp.cliente.HomeCliente;
 import com.example.hotelreservaapp.databinding.AdminhotelFragmentInicioBinding;
 import com.example.hotelreservaapp.databinding.AdminhotelFragmentPerfilBinding;
+import com.google.android.material.button.MaterialButton;
 
 
 public class PerfilFragment extends Fragment {
@@ -21,8 +25,15 @@ public class PerfilFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = AdminhotelFragmentPerfilBinding.inflate(inflater, container, false);
-        return binding.getRoot();
+        View view = inflater.inflate(R.layout.adminhotel_fragment_perfil, container, false);
+
+        //Cerrar sesion
+        MaterialButton btnCerrar = view.findViewById(R.id.btn_cerrar_sesion);
+        btnCerrar.setOnClickListener(v -> {
+            //por ahora directamente al mio bala
+            startActivity(new Intent(getActivity(), MainActivity.class));
+        });
+        return view;
 
     }
 }
