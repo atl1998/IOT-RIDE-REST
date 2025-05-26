aac6be71dcac543c618d57cccad7997b3d23660e
FabricioEstrada <a20210795@pucp.edu.pe>
2025-05-25 19:00:54 -0500

    SolicitarTaxi

--- APP_R_R/app/src/main/java/com/example/hotelreservaapp/NotificacionDetalles.java
+++ APP_R_R/app/src/main/java/com/example/hotelreservaapp/NotificacionDetalles.java
@@ -11,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.hotelreservaapp.Objetos.Notificaciones;
+import com.example.hotelreservaapp.Objetos.NotificacionesStorageHelper;
+import com.example.hotelreservaapp.Objetos.NotificationManagerNoAPP;
 
 public class NotificacionDetalles extends AppCompatActivity {
     private TextView headerNotification, ContentNotification, ContenidoExtra;
@@ -25,6 +27,7 @@ public class NotificacionDetalles extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         imagen = findViewById(R.id.ImageNotificacion);
         headerNotification = findViewById(R.id.headerNotification);
         ContentNotification = findViewById(R.id.ContentNotification);
@@ -33,6 +36,28 @@ public class NotificacionDetalles extends AppCompatActivity {
         Notificaciones notificacion = (Notificaciones) getIntent().getSerializableExtra("notificacion");
 
         if (notificacion != null) {
+            // Solo cargar si notification es != null y si is leido es false xq si es true no vale la pena leerlo
+            if (!notificacion.isLeido()){
+                // 1. Cargar lista guardada (si existe)
+                NotificacionesStorageHelper storageHelper = new NotificacionesStorageHelper(this);
+                Notificaciones[] notificacionesGuardadas = storageHelper.leerArchivoNotificacionesDesdeSubcarpeta();
+
+                // 2. Crear o cargar NotificationManager con la lista actual
+                NotificationManagerNoAPP notificationManagerNoAPP = new NotificationManagerNoAPP();
+                if (notificacionesGuardadas != null && notificacionesGuardadas.length > 0) {
+                    for (Notificaciones n : notificacionesGuardadas) {
+                        if (notificacion.getId()==n.getId()) {
+                            n.setLeido(true);
+                        }
+                        notificationManagerNoAPP.getListaNotificaciones().add(n);
+                    }
+                }
+                // 4. Guardar la lista actualizada
+                Notificaciones[] arregloParaGuardar = notificationManagerNoAPP.getListaNotificaciones()
+                        .toArray(new Notificaciones[0]);
+                storageHelper.guardarArchivoNotificacionesEnSubcarpeta(arregloParaGuardar);
+            }
+
             headerNotification.setText(notificacion.getTituloAmigable());
             ContentNotification.setText(notificacion.getMensaje());
             ContenidoExtra.setText(notificacion.getMensajeExtra());
@@ -40,6 +65,8 @@ public class NotificacionDetalles extends AppCompatActivity {
                 imagen.setImageResource(R.drawable.checkout_image_v2);
             } else if ("02".equals(notificacion.getTipo())) {
                 imagen.setImageResource(R.drawable.checkout_sucessfull);
+            } else if ("03".equals(notificacion.getTipo())) {
+                imagen.setImageResource(R.drawable.taxi);
             }
         }
     }
