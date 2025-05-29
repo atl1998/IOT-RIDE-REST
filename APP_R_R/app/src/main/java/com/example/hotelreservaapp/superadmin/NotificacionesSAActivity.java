c6a825dc03531dd6636ab1b3984cc67569dad920
Daniel <a20212591@pucp.edu.pe>
2025-05-29 02:23:28 -0500

    Ola. Avance notificaciones SuperAdmin y manejo foto de perfil :D

--- APP_R_R/app/src/main/java/com/example/hotelreservaapp/superadmin/NotificacionesSAActivity.java
+++ APP_R_R/app/src/main/java/com/example/hotelreservaapp/superadmin/NotificacionesSAActivity.java
@@ -1,37 +1,22 @@
 package com.example.hotelreservaapp.superadmin;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.hotelreservaapp.MainActivity;
-import com.example.hotelreservaapp.R;
 import java.util.List;
+
+import com.example.hotelreservaapp.R;
 import com.example.hotelreservaapp.databinding.SuperadminNotificacionesBinding;
 import com.example.hotelreservaapp.model.Notificacion;
 import com.example.hotelreservaapp.room.AppDatabase;
+import com.google.android.material.button.MaterialButton;
 
-import java.util.List;
-
-public class NotificacionesActivity extends AppCompatActivity {
+public class NotificacionesSAActivity extends AppCompatActivity {
 
     private SuperadminNotificacionesBinding binding;
-    private NotificacionAdapter adapter;
+    private NotificacionSAAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +29,12 @@ public class NotificacionesActivity extends AppCompatActivity {
         // ✅ Configura RecyclerView
         binding.recyclerNotificaciones.setLayoutManager(new LinearLayoutManager(this));
 
+        binding.backButton.setOnClickListener(v -> finish());
+
         // ✅ Carga las notificaciones desde Room
         List<Notificacion> lista = AppDatabase.getInstance(this).notificacionDao().obtenerTodas();
 
-        adapter = new NotificacionAdapter(lista);
+        adapter = new NotificacionSAAdapter(lista, this);
         binding.recyclerNotificaciones.setAdapter(adapter);
     }
 }
